8195219ecf1d418b8a4c946d8520fc2da080e0ef
FlYpPe <flype360360@gmail.com>
2025-11-24 20:30:59 -0600

    chore: added redirect endpoint test

--- tests/test_create_url.py
+++ tests/test_create_url.py
@@ -15,3 +15,15 @@ def test_create_short_url(client):
 
     assert "short_code" in data
     assert data["original_url"] == "https://example.com/"
+
+def test_redirect():
+    payload = {
+        "original_url": "https://example.com/"
+    }
+    create_response = client.post("/url/", json = payload)
+    assert create_response.status_code == 200
+    response_data = create_response.json()
+    short_url = response_data["short_code"]
+    redirect_response = client.get(f"/{short_url}", follow_redirects=False)
+    assert redirect_response.status_code in (302, 307)
+    assert redirect_response.headers["location"] == payload["original_url"]
